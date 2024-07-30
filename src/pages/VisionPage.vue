6ae7cb2f6c869b906e0f60a7810eda79f9b5c78f
leo <ln777764@gmail.com>
2024-07-31 02:21:53 +0800

    add using native android camera feature

--- src/pages/VisionPage.vue
+++ src/pages/VisionPage.vue
@@ -5,6 +5,8 @@
       @added="onImageAdded"
       accept="image/*"
     />
+    <q-btn @click="takePicture" label="Take Picture" color="primary" />
+    <imq :src="imggsrc" />
     <div v-if="detectedIngredients.length">
       <h3>Detected Ingredients:</h3>
       <ul>
@@ -21,12 +23,29 @@ import roboflowService from 'src/services/roboflowService';
 import api from 'src/api';
 import { useQuasar } from 'quasar';
 import { useRouter } from 'vue-router';
+import { Camera, CameraResultType } from '@capacitor/camera'
 
 export default {
   setup() {
     const detectedIngredients = ref([]);
     const $q = useQuasar();
     const router = useRouter();
+
+    const imggsrc = ref('');
+    async function takePicture() {
+      const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Uri
+      });
+      imggsrc.value = image.webPath;
+      try {
+      const detectionResult = await roboflowService.detectIngredients(image);
+      detectedIngredients.value = detectionResult.predictions;
+      } catch (error) {
+      console.error('Error detecting ingredients:', error);
+      }
+    }
 
     const onImageAdded = async (files) => {
       try {
@@ -64,6 +83,8 @@ export default {
       onImageAdded,
       detectedIngredients,
       generateRecipe,
+      takePicture,
+      imggsrc
     };
   }
 };
