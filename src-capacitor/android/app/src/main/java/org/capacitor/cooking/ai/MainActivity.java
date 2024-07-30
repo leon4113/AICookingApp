695051d84244459edbedd066b0ca447a55cff089
leo <ln777764@gmail.com>
2024-07-31 02:01:41 +0800

    test1 android success

---
+++ src-capacitor/android/app/src/main/java/org/capacitor/cooking/ai/MainActivity.java
@@ -0,0 +1,5 @@
+package org.capacitor.cooking.ai;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000', // Adjust this URL based on your backend server
+  baseURL: 'http://10.19.35.144:3000', // Adjust this URL based on your backend server
   headers: {
     'Content-Type': 'application/json',
   },
