10f48e2dc8d116c2a5eb78401d6e1d0cc7a3487a
leo <ln777764@gmail.com>
2024-09-07 22:12:50 +0800

    test deploy

---
+++ dist/spa/assets/api.52e83815.js
@@ -0,0 +1,1 @@
+import{a}from"./axios.bf56c3c5.js";const t=a.create({baseURL:"http://localhost:3000",headers:{"Content-Type":"application/json"}});export{t as a};
---
+++ dist/spa/assets/use-quasar.eedf8ecf.js
@@ -0,0 +1,1 @@
+import{i as a,Y as r}from"./index.bd582081.js";function u(){return a(r)}export{u};
